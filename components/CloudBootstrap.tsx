186ba6d2e429a99012d6350f841ba82be22b0031
mcb264 <michelbiard05@gmail.com>
2026-08-12 23:32:32 +0200

    Synchroniser automatiquement les écritures locales

--- components/CloudBootstrap.tsx
+++ components/CloudBootstrap.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import { useEffect } from "react";
-import { hydrateFromCloud } from "@/lib/cloud-sync";
+import { hydrateFromCloud, queueCloudSync } from "@/lib/cloud-sync";
 
 export default function CloudBootstrap() {
   useEffect(() => {
+    const originalSetItem = Storage.prototype.setItem;
+    Storage.prototype.setItem = function (key: string, value: string) {
+      originalSetItem.call(this, key, value);
+      if (this === window.localStorage && key.startsWith("ibes:")) queueCloudSync();
+    };
+
     hydrateFromCloud().then((changed) => {
       if (changed) window.location.reload();
     });
+
+    return () => {
+      Storage.prototype.setItem = originalSetItem;
+    };
   }, []);
   return null;
 }
