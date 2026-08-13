456a360279c522c4fe7a3bcf9f122eb5764096bf
mcb264 <michelbiard05@gmail.com>
2026-08-13 19:04:03 +0200

    Prevent cloud hydration reload loop

--- components/CloudBootstrap.tsx
+++ components/CloudBootstrap.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import { hydrateFromCloud, queueCloudSync } from "@/lib/cloud-sync";
 
+const RELOAD_GUARD = "ibes:cloud-bootstrap-reloaded";
+
 export default function CloudBootstrap() {
   useEffect(() => {
     const originalSetItem = Storage.prototype.setItem;
@@ -12,7 +14,14 @@ export default function CloudBootstrap() {
     };
 
     hydrateFromCloud().then((changed) => {
-      if (changed) window.location.reload();
+      if (!changed) {
+        window.sessionStorage.removeItem(RELOAD_GUARD);
+        return;
+      }
+
+      if (window.sessionStorage.getItem(RELOAD_GUARD) === "1") return;
+      window.sessionStorage.setItem(RELOAD_GUARD, "1");
+      window.location.reload();
     });
 
     return () => {
